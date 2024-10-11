fd371b612b482ee4d543b417441869deee05fe72
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-10-11 22:16:39 +0530

    Added all basic code files of java.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // taking input
+        float marks = 77.7f;
+        double marks1 = 92.3328952583497;
+        long marks2 = 9283785208230942L;
+        boolean check = false;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
---
+++ src/inputs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class inputs {
+    public static void main(String[] args) {
+        int a = 2_34_000_000;
+        System.out.println(a);
+        Scanner input = new Scanner(System.in);
+//        System.out.print("please enter a number: ");
+//        int rollno = input.nextInt();
+//        System.out.println("your roll number is: " + rollno);
+//        String name = input.next();
+//        System.out.println(name);
+//        float marks = input.nextFloat();
+//        System.out.println(marks);
+
+    }
+}
