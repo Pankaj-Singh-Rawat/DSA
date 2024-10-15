217a03227b2cdc1b112bf40444128bb0ed6ff29f
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-10-16 00:37:57 +0530

    Modification of Calc.

--- Calculator.java
+++ Calculator.java
@@ -7,45 +7,44 @@ public class Calculator {
 
         int ans = 0 ;
 
-        while(true){
+        while(true) {
             //take operator as input
             System.out.println("Enter the operator- ");
             char op = in.next().trim().charAt(0);
 
 
-            if ( op == '+' || op == '-' || op == '*' || op == '/' || op == '%' ){
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
                 // input two numbers
                 System.out.println("Enter two numbers: ");
                 int num1 = in.nextInt();
                 int num2 = in.nextInt();
 
-                if (op == '+'){
+                if (op == '+') {
                     ans = num1 + num2;
                 }
-                if (op == '-'){
+                if (op == '-') {
                     ans = num1 - num2;
                 }
-                if (op == '*'){
+                if (op == '*') {
                     ans = num1 * num2;
                 }
-                if (op == '/'){
-                    if(num2 != 0){
+                if (op == '/') {
+                    if (num2 != 0) {
                         ans = num1 / num2;
-                    } else{
+                    } else {
                         System.out.println("invalid 2nd input");
                     }
                 }
-                if (op == '%'){
+                if (op == '%') {
                     ans = num1 % num2;
                 }
                 System.out.println("Your answer is : " + ans);
                 System.out.println();
-            } else if (op == 'x' || op == 'X'){
+            } else if (op == 'x' || op == 'X') {
                 break;
-            } else{
+            } else {
                 System.out.println("Invalid operation");
             }
         }
-        System.out.println(ans);
     }
 }
