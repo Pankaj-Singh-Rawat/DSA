5b8da1f2774b4bb107b6d2b05e48c191fa6b93c6
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-10-17 00:10:07 +0530

    swapping in java using functions.

--- swap.java
+++ swap.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class swap {
+    public static void main(String[] args) {
+
+//        int a = 10;
+//        int b = 20;
+
+        //for swapping
+//        int temp = a;
+//        a = b;
+//        b = temp;
+
+        swapping(5,6);
+
+//        System.out.println("a: " + a);
+//        System.out.println("b: " + b);
+
+    }
+
+    static void swapping(int a , int b){
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+
+
 }
