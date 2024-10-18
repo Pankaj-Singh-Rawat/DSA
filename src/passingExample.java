022a1d6a394fca224f18c720fdf8f7506da14bf9
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-10-18 09:35:48 +0530

    Example.

---
+++ src/passingExample.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class passingExample {
+    public static void main(String[] args) {
+//        String name = "Pankaj SIngh Rawat";
+
+        Scanner in = new Scanner(System.in);
+        greet("Shubhjyoti");
+    }
+
+    static void greet(String name) {
+        System.out.println(name);
+    }
+}
